903835f7484f55c5436d54d61bf81fb6e51efe5a
cereallarceny <me@patrickcason.com>
2019-05-05 17:09:27 +0100

    Menu finished and it is glorious...

--- src/menu/index.js
+++ src/menu/index.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { themeGet, borders, display } from 'styled-system';
-// import posed from 'react-pose';
 
 import theme from '../theme';
 import Box from '../box';
@@ -10,9 +9,10 @@ import { CappedText } from '../typography';
 
 const TRACK_SIZE = '2px';
 
-const MenuItem = styled(CappedText)(
+const Menu = styled(Box)({ display: 'inline-block' });
+
+const Item = styled(CappedText)(
   props => ({
-    cursor: 'pointer',
     transition: `color ${themeGet('animations.fast')(
       props
     )} ease-in-out, border ${themeGet('animations.fast')(props)} ease-in-out`
@@ -20,7 +20,23 @@ const MenuItem = styled(CappedText)(
   borders
 );
 
-const Track = styled(Box)(display);
+const MenuItem = React.forwardRef((props, ref) => (
+  <Item {...props} ref={ref} />
+));
+
+const Track = styled(Box)(
+  {
+    height: TRACK_SIZE
+  },
+  display
+);
+
+const Progress = styled(Box)(props => ({
+  height: TRACK_SIZE,
+  transition: `width ${themeGet('animations.fast')(
+    props
+  )} ease-in-out, margin ${themeGet('animations.fast')(props)} ease-in-out`
+}));
 
 const border = `${theme.space[1]}px solid`;
 const getBorder = isSelected =>
@@ -29,48 +45,67 @@ const getBorder = isSelected =>
     : `${border} ${theme.colors.lightGray}`;
 
 export default ({ pages, current, onChange, ...props }) => {
-  if (!current) {
-    current = pages[0].href;
-  }
+  const [selected, setSelected] = useState(null);
+  const [selectedWidth, setSelectedWidth] = useState(0);
+  const [selectedPosition, setSelectedPosition] = useState(0);
 
-  const [selected, setSelected] = useState(current);
+  const menuRef = useRef(null);
+  const itemsRef = pages.map(() => useRef(null));
+
+  const setEverything = index => {
+    const { current } = itemsRef[index];
+    const { href } = pages[index];
+
+    setSelected(href);
+    setSelectedWidth(current.offsetWidth);
+    setSelectedPosition(current.offsetLeft - menuRef.current.offsetLeft);
+  };
 
   useEffect(() => {
-    if (onChange && typeof onChange === 'function') {
+    if (!selected && !selectedWidth && !selectedPosition) {
+      if (!current) {
+        setEverything(0);
+      } else {
+        setEverything(pages.findIndex(p => p.href === current));
+      }
+    }
+
+    if (onChange && typeof onChange === 'function' && selected) {
       onChange(selected);
     }
   }, [selected]);
 
   return (
-    <Box>
-      <Flex {...props} flexDirection={['column', null, 'row']}>
+    <Menu {...props} ref={menuRef}>
+      <Flex flexDirection={['column', null, 'row']}>
         {pages.map(({ title, href }, index) => {
           const isSelected = href === selected;
 
           return (
             <MenuItem
               key={href}
+              ref={itemsRef[index]}
               color={isSelected ? 'primary' : 'lightGray'}
               pl={[3, null, 0]}
               py={[2, null, 0]}
               mb={0}
               borderLeft={[getBorder(isSelected), null, 0]}
               marginRight={index !== pages.length - 1 && [null, null, 4]}
-              onClick={() => setSelected(href)}
+              style={{ cursor: isSelected ? 'default' : 'pointer' }}
+              onClick={() => setEverything(index)}
             >
               {title}
             </MenuItem>
           );
         })}
       </Flex>
-      <Track
-        display={['none', null, 'block']}
-        backgroundColor="snow"
-        pt={TRACK_SIZE}
-        mt={2}
-      >
-        <Box backgroundColor="primary" />
+      <Track display={['none', null, 'block']} backgroundColor="snow" mt={2}>
+        <Progress
+          backgroundColor="primary"
+          width={`${selectedWidth}px`}
+          marginLeft={`${selectedPosition}px`}
+        />
       </Track>
-    </Box>
+    </Menu>
   );
 };
