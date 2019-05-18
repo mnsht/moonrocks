1c9381180ff28868ba5f51d23ab291295aad9024
Thiago Costa Porto <thiagocostaporto@gmail.com>
2019-05-18 14:34:56 +0100

    Rollup bundling

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+
+import pkg from './package.json';
+
+export default {
+  input: 'index.js',
+  external: ['react'],
+  output: [
+    {
+      name: 'sr-components',
+      file: pkg.main,
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        react: 'React'
+      }
+    }
+  ],
+  plugins: [
+    external({
+      includeDependencies: true
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/env', '@babel/preset-react']
+    }),
+    resolve(),
+    uglify()
+  ]
+};
